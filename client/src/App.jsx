fc3de9d03b650cd6c6f02a538144628d1c36917e
satan-pro <sayantancreator@gmail.com>
2025-02-02 01:09:34 +0530

    commit 3.0

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ function App() {
   return (
     <>
       <h1>Tango Django</h1>
+      <p>Ethical Loan Approval System</p>
+      <strong><h1>Wasooli Bhai aa rhe hai!!</h1></strong>
     </>
   )
 }
