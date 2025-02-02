9902e1b5480d4d75b0e187e621baeb3311caaed9
Rishit Mandal <rishit2014@gmail.com>
2025-02-02 12:10:12 +0530

    ui

--- client/src/components/logo.tsx
+++ client/src/components/logo.tsx
@@ -3,8 +3,8 @@ import { cn } from "@/lib/utils"
 export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center gap-1 font-semibold", className)}>
-      <span className="italic font-extrabold text-green-500">Cred</span>
-      <span>Easy</span>
+      <span className="italic font-extrabold text-green-500 text-3xl">Cred</span>
+      <span className="text-3xl">Easy</span>
     </div>
   )
 }
