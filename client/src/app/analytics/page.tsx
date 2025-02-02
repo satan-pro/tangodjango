abdaea19a64bd4b3737b6aaa5f47532b90e8a946
Rishit Mandal <rishit2014@gmail.com>
2025-02-02 12:04:49 +0530

    ui update

--- client/src/app/analytics/page.tsx
+++ client/src/app/analytics/page.tsx
@@ -26,7 +26,7 @@ const stats = [
 
 export default function AnalyticsPage() {
   return (
-    <div className="space-y-4">
+    <div className="space-y-4 p-4 w-[75vw]">
       <div className="flex items-center justify-between space-y-2">
         <h2 className="text-3xl font-bold tracking-tight">Analytics</h2>
       </div>
